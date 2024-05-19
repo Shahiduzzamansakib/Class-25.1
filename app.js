c3bc73699430c2210470b865443268a7029bb874
Shahiduzzamansakib <159687977+Shahiduzzamansakib@users.noreply.github.com>
2024-05-19 17:19:34 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let btn = document.querySelector('.btn');
+
+let body = document.querySelector('body');
+
+btn.addEventListener('click', function(){
+
+   body.classList.toggle('dark-mode');
+   
+
+});
